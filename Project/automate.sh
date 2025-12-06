081bef83cec639259ecc292aee3cdddcf2d2f01b
anujparwal7787 <anujparwal7787@gmail.com>
2025-12-06 17:37:14 +0530

    Made changes to fix some false prints of achives and other logs

--- Project/automate.sh
+++ Project/automate.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 echo "   Linux Build & Deployment Automation System   "
 bash build.sh
-bash package.sh
-bash archive.sh
-bash deploy.sh
-echo "Automation complete."
-echo "$(date) - Full automation cycle finished" >> logs/main_log.log
-
+success_build="$(cat success_build.log)"   
+if [ "$success_build" == "[BUILD] Compilation was successful" ]; then
+    bash package.sh
+    bash archive.sh
+    bash deploy.sh
+    echo "Automation complete."
+    echo "$(date) - Full automation cycle finished" >> logs/main_log.log
+else 
+    echo "Compilation was unsuccessful. Please try again."
+fi 
--- Project/build.sh
+++ Project/build.sh
@@ -3,9 +3,8 @@ echo "[BUILD] Compiling main.c file..."
 mkdir -p build
 gcc src/main.c -o build/main
 if [ $? -eq 0 ]; then
-	echo "[BUILD] Compilation was successful"
+	echo "[BUILD] Compilation was successful" >> logs/success_build.log
+	echo "$(date) - Build has been completed" >> logs/build.log
 else
-	echo "[BUILD] Compilation unsuccessful"
+	echo "[BUILD] Compilation unsuccessful" >> logs/success_build.log
 fi
-echo "$(date) - Build has been completed" >> logs/build.log
-
