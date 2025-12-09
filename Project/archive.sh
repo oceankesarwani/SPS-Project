441bd37db9f37bfd291ccf8d4c4176e6937fd031
anujparwal7787 <anujparwal7787@gmail.com>
2025-12-09 22:05:31 +0530

    Fixed date format

--- Project/archive.sh
+++ Project/archive.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "[ARCHIVE] Archiving old build...:"
 mkdir -p archive
-cp build/main.tar.gz archive/main_$(date+"%d%m%Y%H%M%S").tar.gz
+cp build/main.tar.gz archive/main_$(date +"%d%m%Y%H%M%S").tar.gz
 echo "[ARCHIVE] Archive saved successfully."
 echo "$(date) - Archiving completed." >> logs/archive.log
